cbb42e60e5000bc63da33a65bc2e1c3e3980550f
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2023-10-26 00:20:55 +0530

    Create config_code.py

---
+++ Crash Course/config_code.py
@@ -0,0 +1,12 @@
+!apt-get install openjdk-8-jdk-headless -qq > /dev/null
+!wget -q http://archive.apache.org/dist/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz
+!tar xf spark-3.1.1-bin-hadoop3.2.tgz
+!pip install -q findspark
+import os
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+os.environ["SPARK_HOME"] = "/content/spark-3.1.1-bin-hadoop3.2"
+import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+spark
