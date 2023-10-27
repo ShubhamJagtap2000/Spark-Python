6c038c05480c14df0f5b09cbfc3dd39863b22611
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2023-10-28 00:27:35 +0530

    Update 02- inspect_dataframe.py

--- Crash Course/Core Concepts/02- inspect_dataframe.py
+++ Crash Course/Core Concepts/02- inspect_dataframe.py
@@ -1,1 +1,9 @@
+df.head(n)                        # Gives you the first 'n' rows of a spark dataframe
 
+df.describe().SHOW()              # Gives you statistical description of a dataframe(count, min, max, avg, stddev, etc)
+
+df.columns                        # Columns in a dataframe
+
+df.count()                        # Total no. of rows in a dataframe
+
+df.distinct().count()             # No. of distinct records in your dataframe
